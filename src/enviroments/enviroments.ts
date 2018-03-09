0da9cdedc2fb4c9482a1c9cabb39441e03316ba6
airtongodoy <dev@airtongodoy.com.br>
2018-03-08 23:02:25 -0300

    Ajuste Firebase

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyAUlDR8TxcE7KA9BeSr78tTlTOkw-pJ990",
-        authDomain: "ionicfirebaseautenticate.firebaseapp.com",
-        databaseURL: "https://ionicfirebaseautenticate.firebaseio.com",
-        projectId: "ionicfirebaseautenticate",
-        storageBucket: "ionicfirebaseautenticate.appspot.com",
-        messagingSenderId: "362645811146"
+        apiKey: "API_KEY",
+        authDomain: "DOMAIN",
+        databaseURL: "URL",
+        projectId: "PROJETO_ID",
+        storageBucket: "STORGE",
+        messagingSenderId: "MSG"
     }
 
 };
